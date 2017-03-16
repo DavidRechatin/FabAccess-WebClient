facfd87711b91376a75ee3d8dc1aac9473056658
Pierre 'Civil' Penciolelli <ppenciolelli@gmail.com>
2017-03-16 16:05:16 +0100

    16:05 - Added PHP session fonctions

--- HeadPage.php
+++ HeadPage.php
@@ -1,22 +1,22 @@
+<!-- SESSION START FUNCTION -->
 <?php
-    // On démarre la session (ceci est indispensable dans toutes les pages de notre section membre)
-    session_start ();
-
-    // On récupère nos variables de session
+    session_start ()
     if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) {
-
-    	// On teste pour voir si nos variables ont bien été enregistrées
     	echo '<html>';
     	echo '<head>';
     	echo '<title>Page de notre section membre</title>';
     	echo '</head>';
-
     	echo '<body>';
     }
     else {
     	echo 'Les variables ne sont pas déclarées.';
     }
-    ?>
+?>
+
+
+
+
+<!-- HTML PART -->
 
 <!DOCTYPE html>
 
@@ -25,82 +25,56 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestionnaire</title>
-	<link rel="shortcut icon" href="src/x.png">
+    <title>Gestionnaire OpenFactory</title>
 	<link href="src/style.css" rel="stylesheet" type="text/css" media="all">
 	<link href="src/css.css" rel="stylesheet" type="text/css">
 	<link href="src/police.css" rel="stylesheet" type="text/css">
 </head>
 
 <body class="scroll-assist nav-is-overlay">
-
 	<div class="nav-container bg-dark">
 		<nav class="absolute transparent bg-dark fixed outOfSight scrolled">
 			<div class="nav-bar">
 				<div class="module left">
-					<a href="HeadPage.php">
-						<img class="logo" alt="logo" src="src/OpenFactory.png">
-					</a>
+				    <img class="logo" alt="logo" src="src/OpenFactory.png">
 				</div>
-
-
-
-				<div class="module widget-handle mobile-toggle right visible-sm visible-xs">
-					<i class="ti-menu"></i>
-				</div>
-
+                
 				<div class="module-group">
 					<div class="module widget-handle language">
 						<ul class="menu">
 							<li class="#">
-							<a href="adherents.php">Adhérents</a>
-								<ul>
-									<li>
-										<a href="#">Ajouter</a>
-                                        <a href="#">Modifier</a>
-									</li>
-								</ul>
+							 <a href="adherents.php">Adhérents</a>
 							</li>
                             <li class="#">
-							<a href="equipements.php">Equipements</a>
-								<ul>
-									<li>
-										<a href="#">Status</a>
-									</li>
-								</ul>
+							 <a href="equipments.php">Equipements</a>
 							</li>
                             <li class="#">
-							<a href="incidents.php">Incidents</a>
-								<ul>
-									<li>
-										<a href="#">Status</a>
-                                        <a href="#">Gérer droits</a>
-									</li>
-								</ul>
+							 <a href="incidents.php">Incidents</a>
 							</li>
                             <li class="#">
-							<a href="statistiques.php">Statistiques</a>
-								<ul>
-									<li>
-										<a href="#">Status</a>
-                                        <a href="#">Gérer droits</a>
-									</li>
-								</ul>
+							 <a href="statistiques.php">Statistiques</a>
 							</li>
-							<li class="#">
-							<a href="contact.php">Contact</a>
-								<ul>
-									<li>
+                            <li class="#">
+							 <a href="contact.php">Contact</a>
+							</li>
 						</ul>
 					</div>
 				</div>
                 <div class="module right">
-					<a href="deconnexion.php" class="deconnexion">Deconnexion</a>
+					<a href="deconnexion.php">Deconnexion</a>
 				</div>
 			</div>
 		</nav>
 	</div>
-
+    
+    
+    <!---------
+    CONTINUER ICI
+    ------------>
+    
+    
+    
+    
 
 	<script src="src/jQuery.js"></script>
 	<script src="src/Bootstrap.js"></script>
