3dd057f57f873d1dc6dd47690393a23322f04174
Narboux-Antoine <n42ant@gmail.com>
2017-03-16 14:04:13 +0100

    16/03/17 14h04 import site

---
+++ HeadPage.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestionnaire</title>
+	<link rel="shortcut icon" href="src/x.png">
+	<link href="src/style.css" rel="stylesheet" type="text/css" media="all">
+	<link href="src/css.css" rel="stylesheet" type="text/css">
+	<link href="src/police.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="scroll-assist nav-is-overlay">
+<?php
+    // On démarre la session (ceci est indispensable dans toutes les pages de notre section membre)
+    session_start ();
+
+    // On récupère nos variables de session
+    if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) {
+
+    	// On teste pour voir si nos variables ont bien été enregistrées
+    	echo '<html>';
+    	echo '<head>';
+    	echo '<title>Page de notre section membre</title>';
+    	echo '</head>';
+
+    	echo '<body>';
+    }
+    else {
+    	echo 'Les variables ne sont pas déclarées.';
+    }
+    ?>	
+	<div class="nav-container bg-dark">
+		<nav class="absolute transparent bg-dark fixed outOfSight scrolled">
+			<div class="nav-bar">
+				<div class="module left">
+					<a href="#">
+						<img class="logo" alt="logo" src="src/OpenFactory.png">
+					</a>
+				</div>
+                
+                
+                
+				<div class="module widget-handle mobile-toggle right visible-sm visible-xs">
+					<i class="ti-menu"></i>
+				</div>
+                
+				<div class="module-group">
+					<div class="module widget-handle language">
+						<ul class="menu">
+							<li class="#">
+							<a href="#">Adhérents</a>
+								<ul>
+									<li> 
+										<a href="#">Ajouter</a>
+                                        <a href="#">Modifier</a>
+									</li>
+								</ul>
+							</li>
+                            <li class="#">
+							<a href="#">Equipements</a>
+								<ul>
+									<li> 
+										<a href="#">Status</a>
+									</li>
+								</ul>
+							</li>
+                            <li class="#">
+							<a href="#">Incidents</a>
+								<ul>
+									<li> 
+										<a href="#">Status</a>
+                                        <a href="#">Gérer droits</a>
+									</li>
+								</ul>
+							</li>
+                            <li class="#">
+							<a href="#">Statistiques</a>
+								<ul>
+									<li> 
+										<a href="#">Status</a>
+                                        <a href="#">Gérer droits</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+                <div class="module right">
+					<a href="deconnexion.php">Deconnexion</a>
+				</div>
+			</div>
+		</nav>
+	</div>
+
+
+	<script src="src/jQuery.js"></script>
+	<script src="src/Bootstrap.js"></script>
+
+
+</body>
+</html>
