d30cf6761bc9ceacd75e79e8ef0f6b9bf9ee3128
Narboux-Antoine <n42ant@gmail.com>
2017-03-16 14:29:51 +0100

    16/03/17 14h30

--- HeadPage.php
+++ HeadPage.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-
-<html lang="fr">
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestionnaire</title>
-	<link rel="shortcut icon" href="src/x.png">
-	<link href="src/style.css" rel="stylesheet" type="text/css" media="all">
-	<link href="src/css.css" rel="stylesheet" type="text/css">
-	<link href="src/police.css" rel="stylesheet" type="text/css">
-</head>
-
-<body class="scroll-assist nav-is-overlay">
 <?php
     // On démarre la session (ceci est indispensable dans toutes les pages de notre section membre)
     session_start ();
@@ -31,55 +16,72 @@
     else {
     	echo 'Les variables ne sont pas déclarées.';
     }
-    ?>	
+    ?>
+
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestionnaire</title>
+	<link rel="shortcut icon" href="src/x.png">
+	<link href="src/style.css" rel="stylesheet" type="text/css" media="all">
+	<link href="src/css.css" rel="stylesheet" type="text/css">
+	<link href="src/police.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="scroll-assist nav-is-overlay">
+
 	<div class="nav-container bg-dark">
 		<nav class="absolute transparent bg-dark fixed outOfSight scrolled">
 			<div class="nav-bar">
 				<div class="module left">
-					<a href="#">
+					<a href="HeadPage.php">
 						<img class="logo" alt="logo" src="src/OpenFactory.png">
 					</a>
 				</div>
-                
-                
-                
+
+
+
 				<div class="module widget-handle mobile-toggle right visible-sm visible-xs">
 					<i class="ti-menu"></i>
 				</div>
-                
+
 				<div class="module-group">
 					<div class="module widget-handle language">
 						<ul class="menu">
 							<li class="#">
-							<a href="#">Adhérents</a>
+							<a href="adherents.php">Adhérents</a>
 								<ul>
-									<li> 
+									<li>
 										<a href="#">Ajouter</a>
                                         <a href="#">Modifier</a>
 									</li>
 								</ul>
 							</li>
                             <li class="#">
-							<a href="#">Equipements</a>
+							<a href="equipements.php">Equipements</a>
 								<ul>
-									<li> 
+									<li>
 										<a href="#">Status</a>
 									</li>
 								</ul>
 							</li>
                             <li class="#">
-							<a href="#">Incidents</a>
+							<a href="incidents.php">Incidents</a>
 								<ul>
-									<li> 
+									<li>
 										<a href="#">Status</a>
                                         <a href="#">Gérer droits</a>
 									</li>
 								</ul>
 							</li>
                             <li class="#">
-							<a href="#">Statistiques</a>
+							<a href="statistiques.php">Statistiques</a>
 								<ul>
-									<li> 
+									<li>
 										<a href="#">Status</a>
                                         <a href="#">Gérer droits</a>
 									</li>
@@ -89,7 +91,7 @@
 					</div>
 				</div>
                 <div class="module right">
-					<a href="deconnexion.php">Deconnexion</a>
+					<a href="deconnexion.php" class="deconnexion">Deconnexion</a>
 				</div>
 			</div>
 		</nav>
